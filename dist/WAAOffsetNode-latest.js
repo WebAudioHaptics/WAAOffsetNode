5744d97ab920c9c9f8a1eb0547d6f317225fbba3
Christian Frisson <christian.frisson@gmail.com>
2016-03-30 16:31:30 +0200

    Fixed ones generator to work with Firefox 45.0.1+ (bugzilla id 1257718)

--- dist/WAAOffsetNode-latest.js
+++ dist/WAAOffsetNode-latest.js
@@ -6,12 +6,34 @@ if (typeof window !== 'undefined') window.WAAOffsetNode = WAAOffsetNode
 var WAAOffsetNode = module.exports = function(context) {
   this.context = context
 
-  // Ones generator
-  this._ones = context.createOscillator()
-  this._ones.frequency.value = 0
-  this._ones.setPeriodicWave(context.createPeriodicWave(
-    new Float32Array([0, 1]), new Float32Array([0, 0])))
-  this._ones.start(0)
+  // Ones generator. We use only a single generator 
+  // for all WAAOfsetNodes in the same AudioContext
+  this._ones = WAAOffsetNode._ones.filter(function(ones) {
+    return ones.context === context
+  })[0]
+  if (this._ones) this._ones = this._ones.ones 
+  else {
+      //this._ones = context.createOscillator()
+      //this._ones.frequency.value = 0
+      //this._ones.setPeriodicWave(context.createPeriodicWave(
+      //  new Float32Array([0, 1]), new Float32Array([0, 0])))
+      //this._ones.start(0)
+
+     var ones = context.createBuffer(1, 128, context.sampleRate);
+     var arr = ones.getChannelData(0);
+     for (var i = 0; i < arr.length; i++) {
+     	arr[i] = 1;
+     }
+
+     this._ones = context.createBufferSource();
+     this._ones.channelCount = 1;
+     this._ones.channelCountMode = "explicit";
+     this._ones.buffer = ones;
+     this._ones.loop = true;
+
+     this._ones.start(0);
+    WAAOffsetNode._ones.push({ context: context, ones: this._ones })
+  }
 
   // Multiplier
   this._output = context.createGain()
@@ -27,4 +49,6 @@ WAAOffsetNode.prototype.connect = function() {
 WAAOffsetNode.prototype.disconnect = function() {
   this._output.disconnect.apply(this._output, arguments)
 }
+
+WAAOffsetNode._ones = []
 },{}]},{},[1]);
